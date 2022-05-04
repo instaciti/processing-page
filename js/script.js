84bb7d6c27a46140111a6e596f41d92647edfdbf
instaciti <91252113+instaciti@users.noreply.github.com>
2022-05-04 13:18:37 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 $('#carousal').owlCarousel({
     loop:false,
     autoplay:true,
-    autoplayTimeout:4000,
+    mouseDrag:false,
+    touchDrag: false,
+    autoplayTimeout:3500,
     smartSpeed:1500,
     animateIn: 'fadeIn',
     animateOut: 'fadeOut',
-    mouseDrag:true,
     margin:10,
     nav:false,
     dots:false,
@@ -14,4 +15,81 @@ $('#carousal').owlCarousel({
             items:1
         }
     }
-})
+});
+
+(function ($) {
+
+    "use strict";
+
+   
+        $(".typed").typed({
+          strings: [ "Digital transformation of your business has started.",],
+          typeSpeed: 0,
+          showCursor: false,
+        });
+
+        $(".typed2").typed({
+            strings: [ "Initial data is being processed.",],
+            typeSpeed: 0,
+            showCursor: false,
+            startDelay: 4000,
+          });
+        
+          $(".typed3").typed({
+            strings: [ "Your Business Hello is being created.",],
+            typeSpeed: 0,
+            showCursor: false,
+            startDelay: 7000,
+          });
+
+          $(".typed4").typed({
+            strings: [ "Your Website is being created.",],
+            typeSpeed: 0,
+            showCursor: false,
+            startDelay: 10000,
+          });
+
+          $(".typed5").typed({
+            strings: [ "Your QR Identity is being generated.",],
+            typeSpeed: 0,
+            showCursor: false,
+            startDelay: 13500,
+          });
+
+          $(".typed6").typed({
+            strings: [ "Wonderful things are happening to your business.",],
+            typeSpeed: 0,
+            showCursor: false,
+            startDelay: 17500,
+          });
+
+          $(".typed8").typed({
+            strings: [ "InstaCiti transforms your business into a recognizable brand.",],
+            typeSpeed: 0.1,
+            showCursor: false,
+            startDelay: 21500,
+          });
+
+          $(".typed9").typed({
+            strings: [ "Now Get Discovered, Sell More, Grow.",],
+            typeSpeed: 0.1,
+            showCursor: false,
+            startDelay: 22500,
+          });
+
+          $(".typed10").typed({
+            strings: [ "Congratulations",],
+            typeSpeed: 0,
+            showCursor: false,
+            startDelay: 24500,
+          });
+
+          $(".typed11").typed({
+            strings: [ "Your business is now on InstaCiti.",],
+            typeSpeed: 0,
+            showCursor: false,
+            startDelay: 26000,
+          });
+
+})(jQuery);
+
